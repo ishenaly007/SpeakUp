e3032c9cf77dbb374aba788bff625ef30a6850ad
ishenaly007 <ishennarkozuev07@gmail.com>
2025-05-25 17:42:00 +0600

    add html css url for web

--- src/main/java/com/abit8/speakupengbot/config/CorsConfig.java
+++ src/main/java/com/abit8/speakupengbot/config/CorsConfig.java
@@ -7,10 +7,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5500")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost",
+                        "http://localhost:3000",
+                        "http://localhost:4200",
+                        "http://localhost:8084",
+                        "http://localhost:5500",
+                        "http://127.0.0.1",
+                        "http://127.0.0.1:3000",
+                        "http://127.0.0.1:5500"
+                )
+                .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
